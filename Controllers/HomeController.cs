73085729189cb050db99160bbfbdf012a18c51d7
ManoharRaju93 <manohar.r.raju@gmail.com>
2024-10-20 19:33:03 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspnetCoresControllers_1.Controllers
+{
+    public class HomeController
+    {
+     [Route("home")]
+      [Route("/")]
+        public string method1()
+        {
+            return "hello from the controller method";
+        }
+    }
+}
